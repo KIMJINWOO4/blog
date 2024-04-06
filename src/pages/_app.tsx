497f82ef190e5fe93b8b28b7aca89aa382baa38f
kimjinwoo <rlawlsdn177@naver.com>
2024-04-06 13:48:52 +0900

    style: font-family 적용
    - safari에서 font 적용
    - chrome에서 font 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,9 +2,24 @@ import '../styles/globals.css';
 import '../styles/prose.css';
 import '../styles/highlight-js/github.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <title>K - log</title>
+                <meta name='description' content='열심히 배우겠습니다.' />
+                <link rel='icon' href='../components/icons/favicon-16x16.png' sizes='any' />
+            </Head>
+            <main className={inter.className + 'dark: bg-zinc-700'}>
+                <Component {...pageProps} />;
+            </main>
+        </>
+    );
 }
 
 export default MyApp;
