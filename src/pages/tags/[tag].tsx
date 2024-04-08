756c8136da84d4d6989900e638921e243ea6df72
kimjinwoo <rlawlsdn177@naver.com>
2024-04-09 01:46:46 +0900

    fix: tags title 중복 제거

--- src/pages/tags/[tag].tsx
+++ src/pages/tags/[tag].tsx
@@ -24,11 +24,6 @@ const TagPage: NextPage<TagPageProps> = ({ filteredPosts }) => {
             exit='exit'
             className='flex flex-col min-h-screen bg-white dark:bg-zinc-700'
         >
-            <Head>
-                <title>Tags -{tag}</title>
-                <meta name='description' content='열심히 배우겠습니다.' />
-                <meta name='google-site-verification' content='jPTIFNVfyPOTm8WUaEHm9XtinouRCPGnGOUDKdx9Szc' />
-            </Head>
             <Header />
             <div className='flex-grow container mx-auto p-4'>
                 <motion.h1
