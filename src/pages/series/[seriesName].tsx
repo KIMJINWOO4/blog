5e40b1686b30e243bb7634c810d070830edabd52
kimjinwoo <rlawlsdn177@naver.com>
2024-08-27 21:44:08 +0900

    style: 글 소개 여백 조정

--- src/pages/series/[seriesName].tsx
+++ src/pages/series/[seriesName].tsx
@@ -43,12 +43,41 @@ const SeriesPostsPage: NextPage<SeriesPostsPageProps> = ({ posts }) => {
                         <motion.li
                             key={post.id}
                             variants={fadeInUp}
-                            className='bg-gray-100 dark:bg-zinc-600 p-4 rounded-lg hover:drop-shadow-base-bold'
+                            className='bg-gray-100 dark:bg-zinc-600 p-4 pb-2 rounded-lg hover:drop-shadow-base-bold'
                         >
                             <Link href={`/posts/${post.id}`}>
                                 <div className='text-xl font-semibold dark:text-white'>{post.title}</div>
                             </Link>
-                            <p className='text-gray-600 dark:text-gray-300'>{post.date}</p>
+                            <div className='flex items-center text-gray-500 dark:text-gray-300'>
+                                <svg
+                                    xmlns='http://www.w3.org/2000/svg'
+                                    fill='none'
+                                    viewBox='0 0 24 24'
+                                    strokeWidth='1.5'
+                                    stroke='currentColor'
+                                    width='14'
+                                    height='14'
+                                    className='mr-2' // 아이콘과 날짜 사이 간격 조정
+                                >
+                                    <path
+                                        strokeLinecap='round'
+                                        strokeLinejoin='round'
+                                        d='M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5'
+                                    ></path>
+                                </svg>
+                                <p className='text-gray-600 dark:text-gray-300'>{post.date}</p>
+                            </div>
+                            <div className='flex flex-wrap'>
+                                {post.tags.map((tag, index) => (
+                                    <Link
+                                        key={index}
+                                        href={`/tags/${tag}`}
+                                        className='flex mr-2 items-center rounded-lg transition-all dark:text-zinc-300 dark:hover:bg-zinc-800 px-2 py-1 mb-2 ring-1 dark:bg-zinc-700 ring-neutral-300 dark:ring-neutral-600 font-mono text-sm'
+                                    >
+                                        {tag}
+                                    </Link>
+                                ))}
+                            </div>
                         </motion.li>
                     ))}
                 </motion.ul>
